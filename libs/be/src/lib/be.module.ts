16818a8add672fff2571b98f4d3feb3de7e7c9b8
datienza1991 <datienza@doehle-mnl.com>
2022-07-07 22:46:26 +0800

    add swagger on api

--- libs/be/src/lib/be.module.ts
+++ libs/be/src/lib/be.module.ts
@@ -2,23 +2,18 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from '../users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '../users/entities/user.entity';
-<<<<<<< HEAD
 import { Project } from '../todos/projects/entities/project.entity';
-=======
->>>>>>> 8cba84416192122ccd2438ac30b8adb1b02f22d0
+import { ProjectsModule } from '../todos/projects/projects.module';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'todo-promodoro-db',
-<<<<<<< HEAD
       entities: [User,Project],
-=======
-      entities: [User],
->>>>>>> 8cba84416192122ccd2438ac30b8adb1b02f22d0
       synchronize: true,
     }),
     UsersModule,
+    ProjectsModule
   ],
   exports: [],
 })
