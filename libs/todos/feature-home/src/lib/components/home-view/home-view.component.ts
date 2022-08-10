2f94b59acde5081c968c3d5df3e2a9fb5ce29ee7
datienza1991 <datienza@doehle-mnl.com>
2022-08-10 14:35:28 +0800

    update new project input box

---
+++ libs/todos/feature-home/src/lib/components/home-view/home-view.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'todos-home-view',
+  templateUrl: './home-view.component.html',
+  styleUrls: ['./home-view.component.less'],
+})
+export class HomeViewComponent {
+  name = '';
+  constructor() {
+    // Intialized imports here
+  }
+}
