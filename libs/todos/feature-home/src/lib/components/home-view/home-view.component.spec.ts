8e7ac7127fe36cdc5b3aa60a54ae2db3d849c47e
datienza1991 <datienza@doehle-mnl.com>
2022-08-12 13:02:14 +0800

    Add e2e and unit test

--- libs/todos/feature-home/src/lib/components/home-view/home-view.component.spec.ts
+++ libs/todos/feature-home/src/lib/components/home-view/home-view.component.spec.ts
@@ -19,4 +19,21 @@ describe('HomeViewComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('#onAddProject() should add new #projects', () => {
+    expect(component.projects.length).toEqual(2);
+    component.onAddProject();
+    expect(component.projects.length).toEqual(3);
+  });
+
+  it('#onAddProject() should add new #projects with project 3', () => {
+    expect(component.projects[component.projects.length - 1].name).toContain(
+      'project 2'
+    );
+    component.projectName = 'project 3';
+    component.onAddProject();
+    expect(component.projects[component.projects.length - 1].name).toContain(
+      'project 3'
+    );
+  });
 });
