dfb8d43f15109644e95d362d1509be54954d53ec
Darren Atienza <datienza@doehle-mnl.com>
2022-11-19 23:17:28 +0800

    fix conflicts

--- libs/be/src/todos/projects/projects.module.ts
+++ libs/be/src/todos/projects/projects.module.ts
@@ -1,27 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 import { DatabaseModule } from '../../data/database.module';
 import { projectProviders } from './project.providers';
 
 @Module({
   imports: [DatabaseModule],
-=======
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Project } from './entities/project.entity';
-
-@Module({
-  imports: [TypeOrmModule.forFeature([Project])],
->>>>>>> Stashed changes
-=======
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Project } from './entities/project.entity';
-
-@Module({
-  imports: [TypeOrmModule.forFeature([Project])],
->>>>>>> Stashed changes
   controllers: [ProjectsController],
   providers: [...projectProviders, ProjectsService],
 })
