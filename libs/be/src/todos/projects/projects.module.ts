ed806ce41cb51ee44beb69161725148c93fb00ac
Darren Atienza <datienza@doehle-mnl.com>
2022-11-19 23:06:24 +0800

    add new way of datasource type orm

--- libs/be/src/todos/projects/projects.module.ts
+++ libs/be/src/todos/projects/projects.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
+import { DatabaseModule } from '../../data/database.module';
+import { projectProviders } from './project.providers';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [ProjectsController],
-  providers: [ProjectsService],
+  providers: [...projectProviders, ProjectsService],
 })
 export class ProjectsModule {}
