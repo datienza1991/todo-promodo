cd06a39a1591c40f854c29b25d2620abf88119df
Darren Atienza <92292276+datienza1991@users.noreply.github.com>
2022-11-20 21:27:40 +0800

    Add project service testing (#17)
    * project service testing
    
    * fix conflicts
    
    * update test

--- libs/be/src/todos/projects/projects.module.ts
+++ libs/be/src/todos/projects/projects.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
 import { ProjectsController } from './projects.controller';
-import { DatabaseModule } from '../../data/database.module';
-import { projectProviders } from './project.providers';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Project } from './entities/project.entity';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [TypeOrmModule.forFeature([Project])],
   controllers: [ProjectsController],
-  providers: [...projectProviders, ProjectsService],
+  providers: [ProjectsService],
 })
 export class ProjectsModule {}
